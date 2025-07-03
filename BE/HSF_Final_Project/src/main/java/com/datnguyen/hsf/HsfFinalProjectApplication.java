5e2c18e5f9abd31a8cec53d2628fd075ddbd7712
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-07-03 18:58:03 +0700

    Create Projct

---
+++ BE/HSF_Final_Project/src/main/java/com/datnguyen/hsf/HsfFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.datnguyen.hsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsfFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsfFinalProjectApplication.class, args);
+    }
+
+}
---
+++ BE/HSF_Final_Project/src/test/java/com/datnguyen/hsf/HsfFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datnguyen.hsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsfFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
